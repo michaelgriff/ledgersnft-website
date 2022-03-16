7089d4da31f1f23b29109f4e942bbad1abdeffdb
Michael Griff <michaelgriff55@gmail.com>
2022-03-16 19:01:06 -0400

    change url '

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mint" element={<Mint />} />
+        <Route path="/minttheledge" element={<Mint />} />
       </Routes>
     </Router>
   );
